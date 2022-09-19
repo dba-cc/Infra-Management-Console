07c683cfb95c9241b0ea7bc7d82e9227ae4f6afd
VaibhavBhardawajs <bvaibhav9996@gmail.com>
2022-09-19 18:41:43 +0530

    Model Created.

---
+++ MSUISApi/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using MSUISApi.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace MSUISApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        SqlConnection Con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["MSUISConnectionString"].ConnectionString);
+        SqlDataAdapter Da = new SqlDataAdapter();
+        DataTable Dt = new DataTable();
+        DateTime datetime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.Now.ToUniversalTime(), TimeZoneInfo.FindSystemTimeZoneById("India Standard Time"));
+
+        [HttpPost]
+        public HttpResponseMessage CreateUser(User ObjFac)
+        {
+            try
+            {
+                Int32 Id = Convert.ToInt32(ObjFac.Id);
+                string UserName = Convert.ToString(ObjFac.UserName);
+                string UserPass = Convert.ToString(ObjFac.UserPass);
+                SqlCommand cmd = new SqlCommand("CreateUser", Con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", Id);
+                cmd.Parameters.AddWithValue("@UserName", UserName);
+                cmd.Parameters.AddWithValue("@UserPass", UserPass);
+                cmd.Parameters.AddWithValue("@CreatedOn", datetime);
+                cmd.Parameters.Add("@Message", SqlDbType.NVarChar, 500);
+                cmd.Parameters["@Message"].Direction = ParameterDirection.Output; Con.Open();
+                cmd.ExecuteNonQuery();
+                string strMessage = Convert.ToString(cmd.Parameters["@Message"].Value);
+                Con.Close();
+                if (string.Equals(strMessage, "TRUE"))
+                {
+                    strMessage = "Your data has been added successfully.";
+                }
+                return Return.returnHttp("200", strMessage.ToString(), null);
+            }
+            catch (Exception e)
+            {
+                return Return.returnHttp("201", e.Message, null);
+            }
+        }
+    }
+}
---
+++ MSUISApi/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSUISApi.Models
+{
+    public class User
+    {
+        public Int32 Id { get; set; }
+        public string UserName { get; set; }
+        public string UserPass { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+
+    }
+}
