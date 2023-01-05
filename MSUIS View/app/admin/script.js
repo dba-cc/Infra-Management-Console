b6285fd523b74f5859261d58e610d202c7e12d0f
Harsh <kananiharsh007@gmail.com>
2023-01-05 14:24:59 +0530

    Project Cleaned

--- MSUIS View/app/admin/script.js
+++ MSUIS View/app/admin/script.js
@@ -43,70 +43,70 @@ app.config(["$stateProvider", "$urlRouterProvider", "$httpProvider", function (e
             controller: "dashboardCtrl"
         });
 
-    e.state("login", {
-        url: "/login",
-        parent: "plain",
-        templateUrl: "UI/pages/admin/login.html",
-        controller: "loginCtrl"
-    });
+    //e.state("login", {
+    //    url: "/login",
+    //    parent: "plain",
+    //    templateUrl: "UI/pages/admin/login.html",
+    //    controller: "loginCtrl"
+    //});
 
-    e.state("index", {
-        url: "/index",
-        parent: "plain",
-        templateUrl: "UI/pages/admin/login.html",
-        controller: "IndexCtrl"
-    });
+    //e.state("index", {
+    //    url: "/index",
+    //    parent: "plain",
+    //    templateUrl: "UI/pages/admin/login.html",
+    //    controller: "IndexCtrl"
+    //});
 
-    e.state("home", {
-        url: "/home",
-        parent: "dashboard",
-        templateUrl: "UI/pages/admin/home.html",
-        controller: "usersCtrl"
-    });
+    //e.state("home", {
+    //    url: "/home",
+    //    parent: "dashboard",
+    //    templateUrl: "UI/pages/admin/home.html",
+    //    controller: "usersCtrl"
+    //});
 
-    e.state("users", {
-        url: "/users",
-        parent: "dashboard",
-        templateUrl: "UI/pages/admin/users.html",
-        controller: "usersCtrl"
-    });
+    //e.state("users", {
+    //    url: "/users",
+    //    parent: "dashboard",
+    //    templateUrl: "UI/pages/admin/users.html",
+    //    controller: "usersCtrl"
+    //});
 
-    e.state("students", {
-        url: "/students",
-        parent: "dashboard",
-        templateUrl: "UI/pages/admin/student.html",
-        controller: "studentsCtrl"
-    });
-    e.state("MstFacultyDemoAdd", {
-        url: "/MstFacultyDemoAdd",
-        parent: "plain",
-        templateUrl: "UI/pages/admin/MstFacultyDemoAdd.html",
-        controller: "MstFacultyDemoCtrl"
-    });
+    //e.state("students", {
+    //    url: "/students",
+    //    parent: "dashboard",
+    //    templateUrl: "UI/pages/admin/student.html",
+    //    controller: "studentsCtrl"
+    //});
+    //e.state("MstFacultyDemoAdd", {
+    //    url: "/MstFacultyDemoAdd",
+    //    parent: "plain",
+    //    templateUrl: "UI/pages/admin/MstFacultyDemoAdd.html",
+    //    controller: "MstFacultyDemoCtrl"
+    //});
     //e.state("MstFacultyDemoAdd1", {
     //    url: "/MstFacultyDemoAdd1",
     //    parent: "plain",
     //    templateUrl: "UI/pages/admin/MstFacultyDemoAdd1.html",
     //    controller: "MstFacultyDemoCtrl1"
     //});
-    e.state("MstFacultyProgrammeDemo", {
-        url: "/MstFacultyProgrammeDemo",
-        parent: "plain",
-        templateUrl: "UI/pages/admin/MstFacultyProgrammeDemo.html",
-        controller: "MstFacultyProgrammeDemoCtrl"
-    });
-    e.state("InstituteAdd", {
-        url: "/InstituteAdd",
-        parent: "plain",
-        templateUrl: "UI/pages/admin/InstituteAdd.html",
-        controller: "InstituteCtrl"
-    });
-    e.state("InstituteEdit", {
-        url: "/InstituteEdit",
-        parent: "plain",
-        templateUrl: "UI/pages/admin/InstituteEdit.html",
-        controller: "InstituteCtrl"
-    });
+    //e.state("MstFacultyProgrammeDemo", {
+    //    url: "/MstFacultyProgrammeDemo",
+    //    parent: "plain",
+    //    templateUrl: "UI/pages/admin/MstFacultyProgrammeDemo.html",
+    //    controller: "MstFacultyProgrammeDemoCtrl"
+    //});
+    //e.state("InstituteAdd", {
+    //    url: "/InstituteAdd",
+    //    parent: "plain",
+    //    templateUrl: "UI/pages/admin/InstituteAdd.html",
+    //    controller: "InstituteCtrl"
+    //});
+    //e.state("InstituteEdit", {
+    //    url: "/InstituteEdit",
+    //    parent: "plain",
+    //    templateUrl: "UI/pages/admin/InstituteEdit.html",
+    //    controller: "InstituteCtrl"
+    //});
 
     e.state("User", {
         url: "/User",
