1bf795aa3b0a8bb3f3e36a49c9a0bc2bfc3e9754
Gaurav <hey9aurav@gmail.com>
2023-01-10 18:54:00 +0530

    Dashboard UI Updated + AutoBackup UI

---
+++ MSUISApi/Models/BackUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSUISApi.Models
+{
+    public class BackUp
+    {
+        public string database { get; set; }
+
+        public string location { get; set; }
+
+        public string frequency { get; set; }
+
+        public string type { get; set; }
+
+        public string time { get; set; }
+
+        public string day { get; set; }
+    }
+}
