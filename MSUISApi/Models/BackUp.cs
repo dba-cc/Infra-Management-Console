575bf38702f98d6fac54f12932c00a2380305917
Harsh <kananiharsh007@gmail.com>
2023-01-10 20:58:27 +0530

    Mock

--- MSUISApi/Models/BackUp.cs
+++ MSUISApi/Models/BackUp.cs
@@ -7,11 +7,7 @@ namespace MSUISApi.Models
 {
     public class BackUp
     {
-<<<<<<< HEAD
-        public string name { get; set; }
-=======
         public string database { get; set; }
->>>>>>> 1bf795aa3b0a8bb3f3e36a49c9a0bc2bfc3e9754
 
         public string location { get; set; }
 
@@ -19,11 +15,7 @@ namespace MSUISApi.Models
 
         public string type { get; set; }
 
-<<<<<<< HEAD
-        public DateTime time { get; set; }
-=======
         public string time { get; set; }
->>>>>>> 1bf795aa3b0a8bb3f3e36a49c9a0bc2bfc3e9754
 
         public string day { get; set; }
     }
