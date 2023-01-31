9fcd0a4975e68cf4d231d3750937d44a7bf58061
Gaurav <hey9aurav@gmail.com>
2023-01-31 15:50:19 +0530

    Added chart to credential analytics.

--- MSUIS View/app/admin/controller/CredentialAnalytics.js
+++ MSUIS View/app/admin/controller/CredentialAnalytics.js
@@ -17,6 +17,7 @@
                     }, {
                         dataset: response.obj
                     });
+                    $scope.generateChart(response.obj);
                 }
                 hideLoadingScreen();
             })
@@ -26,4 +27,50 @@
             });
     };
 
+    $scope.generateChart = function (data) {
+        var loginNames = data.map(function (obj) {
+            return obj.loginame.trim();
+        });
+        var noOfConnections = data.map(function (obj) {
+            return obj.noofconnections;
+        });
+        var totalNoOfConnections = data.reduce((acc, item) => acc + item.noofconnections, 0);
+        var colors = data.map(function (obj) {
+            return $scope.generateColor(obj.noofconnections, totalNoOfConnections);
+        });
+        const chart = document.getElementById('analytics-chart');
+
+        new Chart(chart, {
+            type: 'doughnut',
+            data: {
+                labels: loginNames,
+                datasets: [{
+                    label: ' # of Connections',
+                    data: noOfConnections,
+                    borderWidth: 1,
+                    backgroundColor: colors,
+                    borderColor: 'rgb(22, 171, 57)',
+                }]
+            },
+            options: {
+                maintainAspectRatio: false,
+                //responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'right',
+                        labels: {
+                            usePointStyle: true,
+                        },
+                    },
+                   
+                }
+            }
+        });
+    }
+
+    $scope.generateColor = function (noOfConnections, totalNoOfConnections) {
+        var opacity = noOfConnections / totalNoOfConnections;
+        var color = "rgba(22, 171, 57, " + opacity + ")";
+        return color;
+    }
 });
