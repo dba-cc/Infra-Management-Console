411de816502805f7c1cb171cd40befc55757c13f
VaibhavBhardawajs <bvaibhav9996@gmail.com>
2023-04-11 15:42:27 +0530

    Backup Database

--- MSUIS View/app/admin/controller/Database.js
+++ MSUIS View/app/admin/controller/Database.js
@@ -1,7 +1,7 @@
 ﻿app.controller('DBCtrl', function ($scope, $http, $rootScope, NgTableParams, $filter) {
     $rootScope.pageTitle = "Manage Databases";
     $scope.DbList = {};
-
+    $scope.dbname=''
     $scope.dropdown = function () {
         $('.ui.dropdown').dropdown();
     }
@@ -98,5 +98,58 @@
                 $scope.getDatabaseList();
                 hideLoadingScreen();
             });
+    }
+    $scope.custom = function (data) {
+        dbname = data;
+        console.log(dbname);
+        $scope.showBkPopup();
+        document.getElementById('newname').value = dbname;
+    }
+    $scope.showBkPopup = function () {
+        $('.addBkPopup').modal({
+            context: '#parent-container',
+            closable: false,
+            onHidden: function () {
+                document.getElementById('newname').value = '';
+                document.getElementById('location').value = '';
+            }
+        }).modal('show');
+    };
+    $scope.hideBkForm = function () {
+        $('.addBkPopup').modal('hide');
+    };
+
+    $scope.backupDB = function () {
+        var location = document.getElementById('location').value;
+        var newname = document.getElementById('newname').value;
+        if (location === null || location === undefined || location === "" || newname === undefined || newname === null || newname === "") {
+            $scope.message = 'Error! File name or Location can\'t be empty.'
+            showMessage($scope.message)
+        }
+        else {
+            showLoadingScreen();
+            $http({
+                method: 'POST',
+                url: 'api/Database/BackupDatabase',
+                data: {
+                    "FrDbName": dbname,
+                    "ToDbName": newname,
+                    "bkLocation": location
+                },
+                headers: { "Content-Type": 'application/json' }
+            })
+
+                .success(function (response) {
+                    showMessage(response.obj);
+                    $scope.getDatabaseList();
+                    hideLoadingScreen();
+                })
+                .error(function (res) {
+                    $rootScope.$broadcast('dialog', "Error", "alert", res.obj);
+                    $scope.getDatabaseList();
+                    hideLoadingScreen();
+                });
+            $scope.hideBkForm();
+        }
     }
 });
