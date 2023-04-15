f13bceb1506afeec016339753f52b3a931c7e72f
Gaurav <hey9aurav@gmail.com>
2023-04-15 16:35:02 +0530

    Create Indexing GUI Completed.

--- MSUIS View/app/admin/controller/Indexing.js
+++ MSUIS View/app/admin/controller/Indexing.js
@@ -2,15 +2,51 @@
     $rootScope.pageTitle = "Index Analytics";
     //$scope.IndexParams = new NgTableParams({}, {});
     $scope.showIndexTableflag = false;
-
+    $scope.IncludedColumns = [];
+    $scope.ConditionalColumns = [];
+    $scope.SelectedColumns = [];
+    $scope.col_type = 'Conditional Columns'
     $scope.dropdownDatabase = function () {
         $('#dbDropdown').dropdown();
     }
     $scope.dropdown = function () {
         $('#formatInputDiv').dropdown();
     }
+    $scope.dropdownColumn = function () {
+        $('#columnDropdown').dropdown();
+    }
 
-    
+    $scope.moveToSelectedList = function (column) {
+        if (column.type == 'where_column') {
+            var index = $scope.ConditionalColumns.indexOf(column);
+            if (index !== -1) {
+                $scope.ConditionalColumns.splice(index, 1);
+                $scope.SelectedColumns.push(column);
+            }
+        } else {
+            var index = $scope.IncludedColumns.indexOf(column);
+            if (index !== -1) {
+                $scope.IncludedColumns.splice(index, 1);
+                $scope.SelectedColumns.push(column);
+            }
+        }
+    };
+
+    $scope.moveToSuggestedList = function (column) {
+        if (column.type == 'where_column') {
+            var index = $scope.SelectedColumns.indexOf(column);
+            if (index !== -1) {
+                $scope.SelectedColumns.splice(index, 1);
+                $scope.ConditionalColumns.push(column);
+            }
+        } else {
+            var index = $scope.SelectedColumns.indexOf(column);
+            if (index !== -1) {
+                $scope.SelectedColumns.splice(index, 1);
+                $scope.IncludedColumns.push(column);
+            }
+        }
+    };
 
     $scope.Percent = false;
     $scope.Sugg = false;
@@ -96,8 +132,9 @@
             });
     }
     $scope.showdeletePopup = function (data) {
-       $scope.obj=data;
+        $scope.obj = data;
         $('.deletePopup').modal({
+            closable: false,
             context: '#parent-container'
         }).modal('show');
     }
@@ -137,7 +174,7 @@
                 hideLoadingScreen();
             });
     }
-   
+
 
     $scope.hidedeletePopup = function () {
         $('.deletePopup').modal('hide');
@@ -155,27 +192,31 @@
         if (data.Index_Advantage < 500) {
             document.getElementById('warning').style.display = 'block'
         }
-        var index=document.getElementById('indexname').value = 'IX_'+data.tablename
+
+        var index = document.getElementById('indexname').value = 'IX_' + data.tablename
         $scope.createformat["indexname"] = index
-        if (data.inequalitycol === '---') {
-           var wr= document.getElementById('EIcolumnsname').value = data.equalitycol
-            $scope.createformat["whereCols"] =wr
-        }
-        else {
-            var wr = document.getElementById('EIcolumnsname').value = data.equalitycol + ',' + data.inequalitycol
-            $scope.createformat["whereCols"] = wr
-        } if (data.inequalitycol === '---') {
-        } else {
-           var col= document.getElementById('columnsname').value = data.includedcol
-            $scope.createformat["includedcol"] = col
-        }
-        console.log($scope.cobj)
+
+        $scope.IncludedColumns = data.includedcol.split(',').map(function (str) {
+            if (str != " ---") {
+                return { 'type': 'include_column', 'column': str.trim() };
+            }
+        });
+        var whereColumns = data.equalitycol + ', ' + data.inequalitycol        
+        whereColumns.split(',').map(function (str) {
+            if (str != " ---") {
+                $scope.ConditionalColumns.push({ 'type': 'where_column', 'column': str.trim() });
+            }
+        });
+        console.log($scope.IncludedColumns);
+        console.log($scope.ConditionalColumns);
         $('.createPopup').modal({
             context: '#parent-container',
+            closable: false,
             onHidden: function () {
+                document.getElementById('warning').style.display = 'none'
                 document.getElementById('indexname').value = '';
-                document.getElementById('EIcolumnsname').value = '';
-                document.getElementById('columnsname').value = '';
+                $scope.SuggestedColumns = [];
+                $scope.SelectedColumns = [];
             }
         }).modal('show');
     }
@@ -191,7 +232,7 @@
                 "whereCols": $scope.createformat["whereCols"],
                 "includedcol": $scope.createformat["includedcol"],
                 "tablename": $scope.cobj.tablename,
-                "onlineFlag":1
+                "onlineFlag": 1
             },
             headers: { "Content-Type": 'application/json' }
         })
@@ -230,36 +271,179 @@
             $('.ui.indicating.progress').progress();
         }, 500);
     }, true);
-    
+
     $scope.GetIndexSugg = function () {
-        showLoadingScreen();
-        $http({
-            method: 'POST',
-            url: 'api/Analytics/GetIndexSuggestions',
-            data: '"' + $scope.Database.name + '"',
-            headers: { "Content-Type": 'application/json' }
-        })
-            .success(function (response) {
-                $rootScope.showLoading = false;
-                if (response.response_code != "200") {
-                    $rootScope.$broadcast('dialog', "Error", "alert", response.obj);
-                }
-                else {
-                    $scope.IndexSuggParams = new NgTableParams({
-                        count: response.obj.length
-                    }, {
-                        dataset: response.obj,
-                    });
-                    $timeout(function () {
-                        $('.ui.indicating.progress').progress();
-                    }, 500);
-                    showIndexTableflag = true
-                }
-                hideLoadingScreen();
-            })
-            .error(function (res) {
-                $rootScope.$broadcast('dialog', "Error", "alert", res.obj);
-                hideLoadingScreen();
-            });
+        var data = [
+            {
+                "tablename": "Particular_Bill_Detail",
+                "equalitycol": "bno",
+                "inequalitycol": "---",
+                "includedcol": "---",
+                "columnname": null,
+                "indexname": null,
+                "seeks": 130,
+                "Index_Advantage": 0.705,
+                "SeekPercentage": 0,
+                "Percent_Red": 32.19
+            },
+            {
+                "tablename": "Particular_Bill_Detail",
+                "equalitycol": "pr",
+                "inequalitycol": "---",
+                "includedcol": "bno",
+                "columnname": null,
+                "indexname": null,
+                "seeks": 116,
+                "Index_Advantage": 0.613,
+                "SeekPercentage": 0,
+                "Percent_Red": 47.13
+            },
+            {
+                "tablename": "Particular_Bill_Detail",
+                "equalitycol": "bno, radio_s_v",
+                "inequalitycol": "---",
+                "includedcol": "---",
+                "columnname": null,
+                "indexname": null,
+                "seeks": 62,
+                "Index_Advantage": 0.34,
+                "SeekPercentage": 0,
+                "Percent_Red": 33.01
+            },
+            {
+                "tablename": "Particular_Bill_Detail",
+                "equalitycol": "bno, pr",
+                "inequalitycol": "---",
+                "includedcol": "---",
+                "columnname": null,
+                "indexname": null,
+                "seeks": 47,
+                "Index_Advantage": 0.296,
+                "SeekPercentage": 0,
+                "Percent_Red": 60.54
+            },
+            {
+                "tablename": "Particular_Bill_Detail",
+                "equalitycol": "bno",
+                "inequalitycol": "Vendor_Account_ID",
+                "includedcol": "---",
+                "columnname": null,
+                "indexname": null,
+                "seeks": 11,
+                "Index_Advantage": 0.057,
+                "SeekPercentage": 0,
+                "Percent_Red": 24.06
+            },
+            {
+                "tablename": "Bill_Detail",
+                "equalitycol": "status, is_deleted",
+                "inequalitycol": "---",
+                "includedcol": "faculty",
+                "columnname": null,
+                "indexname": null,
+                "seeks": 4,
+                "Index_Advantage": 0.015,
+                "SeekPercentage": 0,
+                "Percent_Red": 30.65
+            },
+            {
+                "tablename": "Bill_Detail",
+                "equalitycol": "faculty, is_deleted",
+                "inequalitycol": "---",
+                "includedcol": "bill_type, budget_head, sub_head, owner, date, sanctioned_amount, total_expanditure, audit_sanction, balance, status, reference_no",
+                "columnname": null,
+                "indexname": null,
+                "seeks": 4,
+                "Index_Advantage": 0.012,
+                "SeekPercentage": 0,
+                "Percent_Red": 61.86
+            },
+            {
+                "tablename": "Bill_Detail",
+                "equalitycol": "faculty, status, fr",
+                "inequalitycol": "---",
+                "includedcol": "bill_type, budget_head, officeof, date, total_expanditure, dean_sanction, audit_sanction",
+                "columnname": null,
+                "indexname": null,
+                "seeks": 3,
+                "Index_Advantage": 0.01,
+                "SeekPercentage": 0,
+                "Percent_Red": 33.77
+            },
+            {
+                "tablename": "Bill_Detail",
+                "equalitycol": "status, fr",
+                "inequalitycol": "---",
+                "includedcol": "faculty",
+                "columnname": null,
+                "indexname": null,
+                "seeks": 2,
+                "Index_Advantage": 0.008,
+                "SeekPercentage": 0,
+                "Percent_Red": 33.77
+            },
+            {
+                "tablename": "Bill_Detail",
+                "equalitycol": "audit, account, status, fr, is_deleted",
+                "inequalitycol": "---",
+                "includedcol": "faculty",
+                "columnname": null,
+                "indexname": null,
+                "seeks": 2,
+                "Index_Advantage": 0.008,
+                "SeekPercentage": 0,
+                "Percent_Red": 35.72
+            },
+            {
+                "tablename": "Bill_Detail",
+                "equalitycol": "faculty, account, status, is_deleted",
+                "inequalitycol": "---",
+                "includedcol": "bill_type, budget_head, officeof, date, total_expanditure, dean_sanction, audit_sanction",
+                "columnname": null,
+                "indexname": null,
+                "seeks": 1,
+                "Index_Advantage": 0.003,
+                "SeekPercentage": 0,
+                "Percent_Red": 33.49
+            }
+        ]
+        $scope.IndexSuggParams = new NgTableParams({
+            count: data.length
+        }, {
+            dataset: data,
+        });
+        $timeout(function () {
+            $('.ui.indicating.progress').progress();
+        }, 500);
+        showIndexTableflag = true
+        //showLoadingScreen();
+        //$http({
+        //    method: 'POST',
+        //    url: 'api/Analytics/GetIndexSuggestions',
+        //    data: '"' + $scope.Database.name + '"',
+        //    headers: { "Content-Type": 'application/json' }
+        //})
+        //    .success(function (response) {
+        //        $rootScope.showLoading = false;
+        //        if (response.response_code != "200") {
+        //            $rootScope.$broadcast('dialog', "Error", "alert", response.obj);
+        //        }
+        //        else {
+        //            $scope.IndexSuggParams = new NgTableParams({
+        //                count: response.obj.length
+        //            }, {
+        //                dataset: response.obj,
+        //            });
+        //            $timeout(function () {
+        //                $('.ui.indicating.progress').progress();
+        //            }, 500);
+        //            showIndexTableflag = true
+        //        }
+        //        hideLoadingScreen();
+        //    })
+        //    .error(function (res) {
+        //        $rootScope.$broadcast('dialog', "Error", "alert", res.obj);
+        //        hideLoadingScreen();
+        //    });
     }
 });
